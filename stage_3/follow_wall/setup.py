6b1c41f9e8af51d031579d5fe9b918af6a616e6b
Gokul Edakke Puram <gokul.edakkepuram@hs-weingarten.de>
2025-05-16 02:29:37 +0200

    Rename node

--- stage_3/follow_wall/setup.py
+++ stage_3/follow_wall/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'follow_wall = follow_wall.follow_wall:main'
+            'follow_wall = follow_wall.follow_wall_node:main'
         ],
     },
 )
